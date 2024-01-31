17091ad353e053a449646c4bc2f9f0de4a444e66
vanxLord90 <dasaka.vishnu@gmail.com>
2024-01-31 13:37:58 -0500

    added some last stuff

--- obj/Debug/net8.0/MovieReviewApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieReviewApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026ae6d2be5e988af3c926f7779daa8ebb564e21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ccd87408b8ac7be5ea2876f161d3157081b9db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
